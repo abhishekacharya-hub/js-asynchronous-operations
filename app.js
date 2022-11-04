6a37eefb398ae8c92ff5c160409c787018889c7c
abhishek acharya <85024420+abhishekacharya-hub@users.noreply.github.com>
2022-11-04 21:14:52 +0530

    first commit

--- app.js
+++ app.js
@@ -20,3 +20,36 @@ const promise1 = new Promise((resolve,reject) => {
     console.log(result)
     return result + 30;
 })
+
+//promise.all()
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 10, "first");
+  });
+  const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 20, "second");
+  });
+  
+  Promise.all([promise2, promise3])
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(`error in promise ${error}`);
+    });
+  
+
+//promise.race()
+const promise4 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 10, "first");
+  });
+  const promise5 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 20, "second");
+  });
+  
+  Promise.race([promise4, promise5]).then(function (result) {
+    console.log(result);
+  });
+
+
+
+
